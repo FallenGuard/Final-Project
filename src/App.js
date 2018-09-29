10a5653be166cf4317ea20294de2da9337687e8b
Fallenguard <you@example.com>
2018-09-29 08:30:16 -0600

    Just an update after most Javascript has been put in

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      Changestate: 'This text Will change once the Button is clicked'
+    };
+
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    this.setState({
+      Changestate: 'Yay.'
+    });
+  }
+
   render() {
     return (
-      <div className="App">
+      <div classname="App">
+        <h1>{this.state.Changestate}</h1>
+        <div onClick={this.onClick}>Click Here!</div>
       </div>
     );
   }
 }
 
-export default App;
+export default App
